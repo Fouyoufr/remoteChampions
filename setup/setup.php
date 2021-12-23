1659276e86a7802074f32375ae199bf2b66101eb
Fouyoufr <46448472+Fouyoufr@users.noreply.github.com>
2021-12-23 16:05:00 +0100

    5.4
    Refonte complète du moteur, sans base de donnée.
    (relancer la mise à jour si nécessaire)
    Correction DockerFile (fin des tests)

--- setup/setup.php
+++ setup/setup.php
@@ -281,9 +281,7 @@ foreach ($rcLangs as $boxLang) {
   if (!$newBoxes=simplexml_load_file("$setupSourcePath/$boxLang/boxes.xml")) echo "<div class='error'>".$str['openFileErr'].".<div class='subError'>".$str['gameHelpUp2']." '$setupSourcePath/$boxLang/boxes.xml'.</div></div>";
   else {
     echo "$boxLang - OK.<br/>";
-    foreach($newBoxes->box as $newBox) foreach ($xmlBoxes->box as $oldBox) if ($newBox['id']<>1 and $newBox['id']->__toString()==$oldBox['id']->__toString()) {
-		$newBox['own']=$oldBox['own']->__toString();
-		echo $newBox['id'].': '.$newBox['name'].'('.$newBox['own'].')<br/>';}
+    foreach($newBoxes->box as $newBox) foreach ($xmlBoxes->box as $oldBox) if ($newBox['id']<>1 and $newBox['id']->__toString()==$oldBox['id']->__toString()) $newBox['own']=$oldBox['own']->__toString();
     xmlSave($newBoxes,"$boxLang/boxes.xml");}}
 
 echo "</table></div><div class='pannel'><div class='pannelTitle'>".$str['imgUp']."</div><table><tr><th>".$str['pic']."</th><th></th></tr>";
