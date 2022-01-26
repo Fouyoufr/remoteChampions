b7f3160c4bc9bfcd67917e648cddddb168340e10
Fouyoufr <46448472+Fouyoufr@users.noreply.github.com>
2022-01-26 13:19:59 +0100

    6.0g
    XML Header

--- setup/longpool.php
+++ setup/longpool.php
@@ -1,4 +1,5 @@
 <?php
+header('Content-Type: application/xml; charset=utf-8');
 $startTime=time();
 session_write_close();
 ignore_user_abort(false);
@@ -7,7 +8,7 @@ set_time_limit(60);
 // Récupération de la partie sollicitée
 if (isset($_GET['p'])) $partieFile='ajax/'.strtoupper(htmlspecialchars($_GET['p'])).'.xml';
 if ((isset($partieFile) and !file_exists($partieFile)) or !isset($partieFile)) {
-  echo 'Erreur : Le fichier "'.$partieFile.'" n\'existe pas...';
+  echo '<?xml version="1.0" encoding="UTF-8"?><erreur>Le fichier "'.$partieFile.'" n\'existe pas...</erreur>';
   while (true);}
 $index=0;
 while ($index<30 and !isset($_GET['init'])) {
